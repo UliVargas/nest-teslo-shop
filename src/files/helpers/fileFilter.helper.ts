babab5e31f43ef58a4e255d10bde24da4d2d0ede
Ulises Vargas <uli.vargas02@gmail.com>
2023-11-21 16:17:19 -0600

    quito espacios de los helpers

--- src/files/helpers/fileFilter.helper.ts
+++ src/files/helpers/fileFilter.helper.ts
@@ -1,10 +1,5 @@
-
-
 export const fileFilter = ( req: Express.Request, file: Express.Multer.File, callback: Function ) => {
-
-    // console.log({ file })
     if ( !file ) return callback( new Error('File is empty'), false );
-
 
     const fileExptension = file.mimetype.split('/')[1];
     const validExtensions = ['jpg','jpeg','png','gif'];
